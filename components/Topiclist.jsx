23bc88004cd81f8bb5fe70142a5d32ffef27c018
Nikhil Walia <nikhilwalia@Nikhils-MacBook-Air.local>
2024-06-28 21:17:49 +0530

    version 1

--- components/Topiclist.jsx
+++ components/Topiclist.jsx
@@ -1,55 +1,46 @@
-
 import Link from "next/link";
-import { HiPencilAlt } from "react-icons/hi"
+import { HiPencilAlt } from "react-icons/hi";
 import Removebtn from "./Removebtn";
 
 const getData = async () => {
-  
-    try {
-        const values = await fetch(`${process.env.API_URL}/api/topics`);
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/topics`);
 
-        if (!values.ok) {
-            throw new Error("Errro in fetching data! hello ");
-
-        }
-        return await values.json();
-    } catch (error) {
-        console.log(error);
+    if (!response.ok) {
+      throw new Error("Error in fetching data!");
     }
-}
+    return await response.json();
+  } catch (error) {
+    console.error('Fetch error:', error);
+    return null;
+  }
+};
 
-const Topiclist =  async () => {
-    const {values} = await getData();
-    return (
-        <>
-            {
-                values?.map((v) => {
-                    return (
+const Topiclist = async () => {
+  const data = await getData();
+  
+  if (!data) {
+    return <p>Error in fetching data!</p>;
+  }
 
-                        <div key={v._id} className=" border p-5 mt-2 flex justify-between">
-                            <div className="">
-                                <h1>{v.title}</h1>
-                                <p>
-                                    {v.description}
-                                </p>
-                            </div>
-                            <div className="flex items-start gap-5">
-                                <Removebtn id={v._id} />
-                                <Link href={`/editTodo/${v._id}`}>
-                                    <HiPencilAlt size={24} />
-                                </Link>
-                            </div>
-                        </div>
-
-                    )
-                })
-            }
-
-        </>
-    )
-
-
-
-}
+  return (
+    <>
+      {data?.map((topic) => (
+        <div key={topic._id} className="border p-5 mt-2 flex justify-between">
+          <div>
+            <h1>{topic.title}</h1>
+            <p>{topic.description}</p>
+          </div>
+          <div className="flex items-start gap-5">
+            <Removebtn id={topic._id} />
+            <Link href={`/editTodo/${topic._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default Topiclist;
