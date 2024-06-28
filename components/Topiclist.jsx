52909bc96b3804ad309055463b2ba2d5a9fb9ec3
Nikhil Walia <nikhilwalia@Nikhils-MacBook-Air.local>
2024-06-28 21:34:04 +0530

    version 8

--- components/Topiclist.jsx
+++ components/Topiclist.jsx
@@ -1,44 +1,46 @@
-"use client"
 import Link from "next/link";
 import { HiPencilAlt } from "react-icons/hi";
-import Removebtn from "../components/Removebtn";
+import Removebtn from "./Removebtn";
 
-const Topiclist = ({ topics }) => {
-  return (
-    <>
-      {topics?.map((topic) => (
-        <div key={topic._id} className="border p-5 mt-2 flex justify-between">
-          <div>
-            <h1>{topic.title}</h1>
-            <p>{topic.description}</p>
-          </div>
-          <div className="flex items-start gap-5">
-            <Removebtn id={topic._id} />
-            <Link href={`/editTodo/${topic._id}`}>
-              <HiPencilAlt size={24} />
-            </Link>
-          </div>
-        </div>
-      ))}
-    </>
-  );
+const getData = async() => {
+    try {
+        const response = await fetch(`${process.env.API_URL}/api/topics`);
+
+        if (!response.ok) {
+            throw new Error("Error in fetching data!");
+        }
+        return response.json();
+    } catch (error) {
+        console.error('Fetch error:', error);
+        return null;
+    }
 };
 
-export async function getServerSideProps() {
-  const API_URL = process.env.API_URL;
-  let topics = [];
+const Topiclist = async () => {
+    const data = await getData();
 
-  try {
-    const response = await fetch(`${API_URL}/api/topics`, { cache: "no-store" });
-    if (!response.ok) {
-      throw new Error("Error in fetching data!");
+    if (!data) {
+        return <p>Error in fetching data!</p>;
     }
-    topics = await response.json();
-  } catch (error) {
-    console.error('Fetch error:', error);
-  }
 
-  return { props: { topics } };
-}
+    return (
+        <>
+            {data?.map((topic) => (
+                <div key={topic._id} className="border p-5 mt-2 flex justify-between">
+                    <div>
+                        <h1>{topic.title}</h1>
+                        <p>{topic.description}</p>
+                    </div>
+                    <div className="flex items-start gap-5">
+                        <Removebtn id={topic._id} />
+                        <Link href={`/editTodo/${topic._id}`}>
+                            <HiPencilAlt size={24} />
+                        </Link>
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+};
 
 export default Topiclist;
